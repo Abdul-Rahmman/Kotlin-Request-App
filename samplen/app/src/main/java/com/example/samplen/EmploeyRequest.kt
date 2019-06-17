1b7d875ef3e1ff0f4a5cd039fb37b8feb11623bd
Manee44 <kudabaksh44@gmail.com>
2019-06-17 18:10:53 +0300

    Create EmploeyRequest,MgrLogin,MgrRequest Activities

---
+++ samplen/app/src/main/java/com/example/samplen/EmploeyRequest.kt
@@ -0,0 +1,12 @@
+package com.example.samplen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EmploeyRequest : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emploey_request)
+    }
+}
